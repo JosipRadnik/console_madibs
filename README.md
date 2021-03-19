ca8f7c9f2063dfedcfd2efb73ea3c594ff3a3767
JosipRadnik <soljicjosip1987@gmail.com>
2021-03-19 20:41:51 +0100

    console_madibs - var instead of string, README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 A simple madibs game used for string manipulating practice.
+
+This app doesn't work on VSC terminal. Error: "Global evaluation requires a thread to have been loaded.".
+
+Download the project, and run (through the Windows CMD) "main.dart" file located inside the "lib" folder.
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'dart:io';
 
 void main() {
-  print("Enter a color: ");
-  String color = stdin.readLineSync();
+  print('Enter a color: ');
+  var color = stdin.readLineSync();
 
-  print("Enter a plural noun: ");
-  String noun = stdin.readLineSync();
+  print('Enter a plural noun: ');
+  var noun = stdin.readLineSync();
 
-  print("Enter a celebrity: ");
-  String celebrity = stdin.readLineSync();
+  print('Enter a celebrity: ');
+  var celebrity = stdin.readLineSync();
 
-  print("Roses are ${color}.");
-  print("${noun} are blue.");
-  print("I love ${celebrity}.");
+  print('Roses are ${color}.');
+  print('${noun} are blue.');
+  print('I love ${celebrity}.');
 }
